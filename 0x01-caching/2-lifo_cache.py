9c9cc3d63669e94797476afaeae519653aaf6bc3
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-22 18:47:09 +0300

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,10 +2,10 @@
 """ caching system
     """
 
-BasicCachey = __import__("base_caching").BaseCaching
+from base_caching import BaseCaching
 
 
-class LIFOCache(BasicCachey):
+class LIFOCache(BaseCaching):
     """ caching system:
 
     Args:

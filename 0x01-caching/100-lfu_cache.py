8dabd19061e54ee1dab625e3337148b63991a3cc
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-22 19:05:53 +0300

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -20,16 +20,17 @@ class LFUCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        self.cache_data[key] = item
-        if len(self.cache_data.keys()) > self.MAX_ITEMS:
-            pop = min(self.temp_list, key=self.temp_list.get)
-            self.temp_list.pop(pop)
-            self.cache_data.pop(pop)
-            print(f"DISCARD: {pop}")
-        if not (key in self.temp_list):
-            self.temp_list[key] = 0
-        else:
-            self.temp_list[key] += 1
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = min(self.temp_list, key=self.temp_list.get)
+                self.temp_list.pop(pop)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+            if not (key in self.temp_list):
+                self.temp_list[key] = 0
+            else:
+                self.temp_list[key] += 1
 
     def get(self, key):
         """ Get an item by key

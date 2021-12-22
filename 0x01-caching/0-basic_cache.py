ea3d9b47b1a51fb7083ae98d961ad83fe16d59b5
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-22 18:45:09 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,10 +2,10 @@
 """ caching system
     """
 
-BasicCachey = __import__("base_caching").BaseCaching
+from base_caching import BaseCaching
 
 
-class BasicCache(BasicCachey):
+class BasicCache(BaseCaching):
     """ caching system:
 
     Args:

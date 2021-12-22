9bde6ca6d9ccff5a1f0cf3f01b64735713c149ad
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-22 18:51:56 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-""" caching system
-    """
+"""
+Basic Caching
+"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ caching system:
-
-    Args:
-        BasicCachey ([class]): [basic caching]
+    """
+    Caching class with set and get methods defined
     """
 
     def put(self, key, item):
-        """ Add an item in the cache
         """
-        if key is not None or item is not None:
+        Add an item in the cache
+        """
+        if not (key is None or item is None):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
-            return None
+        Get an item by key
+        """
         return self.cache_data.get(key)

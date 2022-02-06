7994206cc56ba2d2afbbf77782690c72663f7827
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2022-02-06 14:25:48 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -33,6 +33,5 @@ class LRUCache(BaseCaching):
         """
         if (key is None) or not (key in self.cache_data):
             return None
-        self.temp_list.insert(len(self.temp_list),
-                              self.temp_list.remove(key))
+        self.temp_list.remove(key)
         return self.cache_data.get(key)

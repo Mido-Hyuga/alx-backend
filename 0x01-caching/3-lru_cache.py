c108ea5d77430f45b3a3b1f36cfd1bac3fe5f85f
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2022-02-06 14:17:15 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -25,10 +25,7 @@ class LRUCache(BaseCaching):
             self.temp_list.append(key)
             if len(self.cache_data.keys()) > self.MAX_ITEMS:
                 pop = self.temp_list[0]
-                self.temp_list = list(
-                    filter(
-                        lambda x: x != pop,
-                        self.temp_list))
+                self.temp_list.pop(0)
                 self.cache_data.pop(pop)
                 print(f"DISCARD: {pop}")
 
